d90ca9f9da2b527a7170b2d917a792fb6704958f
Kenny Rossi <kpr8402@ad.rit.edu>
2021-02-21 11:38:07 -0500

    Added CollisionManager and Player
    Added 2 scripts, one for managing collisions between player and items shoppers and the other describing the player.

---
+++ Brickhacks2021/Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+    // Declaring fields
+    public List<GameObject> items;
+    public GameObject player;
+    public List<GameObject> shoppers;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for collisions of player between shoppers and items, add items if they collide with player
+        // or take damage if they collide with shoopper
+        for (int i = 0; i < shoppers.Count; i++)
+        {
+            if (Collision(player, shoppers[i]))
+            {
+                // Decrease life of player
+                if (player.GetComponent<Player>() != null)
+                {
+                    player.GetComponent<Player>().DecrementHealth();
+                }
+                // Destroy GameObject and remove it from shoppers array
+                Destroy(shoppers[i]);
+                shoppers.RemoveAt(i);
+                i--;
+            }
+        }
+
+        for (int i = 0; i < items.Count; i++)
+        {
+            if (Collision(player, items[i]))
+            {
+                if (player.GetComponent<Player>() != null)
+                {
+                    player.GetComponent<Player>().AddItem(items[i]);
+                }
+                items.RemoveAt(i);
+                i--;
+            }
+        }
+        
+    }
+
+    // Method to determine collision (AABB method)
+    public bool Collision(GameObject a, GameObject b)
+    {
+        // initialize bool to be returned to false
+        bool collide = false;
+
+        // 4 conditionals check for collision
+        if (b.GetComponent<SpriteRenderer>().bounds.min.x < a.GetComponent<SpriteRenderer>().bounds.max.x && b.GetComponent<SpriteRenderer>().bounds.max.x > a.GetComponent<SpriteRenderer>().bounds.min.x && b.GetComponent<SpriteRenderer>().bounds.max.y > a.GetComponent<SpriteRenderer>().bounds.min.y && b.GetComponent<SpriteRenderer>().bounds.min.y < a.GetComponent<SpriteRenderer>().bounds.max.y)
+        {
+            // set bool to true if collision detected
+            collide = true;
+        }
+
+        // return bool
+        return collide;
+    }
+}
---
+++ Brickhacks2021/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Declaring fields
+    public int lives;
+    public List<GameObject> playerItems;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.lives = 3;
+        playerItems = new List<GameObject>();
+    }
+    // Have player take damage
+    public void DecrementHealth()
+    {
+        this.lives = this.lives - 1;
+    }
+
+    // Add item to players list of items
+    public void AddItem(GameObject item)
+    {
+        this.playerItems.Add(item);
+    }
+}
